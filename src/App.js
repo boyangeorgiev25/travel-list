c08e018571270b58c7f92477deef255e6f71219d
boyan <106580794+bobyTopG@users.noreply.github.com>
2025-07-09 11:13:24 +0300

    sorting function implemented

--- src/App.js
+++ src/App.js
@@ -71,13 +71,38 @@ const [quantity, setQuantity] = useState(1);
 }
 
 function PackingList({ items , onDelete, itemToggle }) {
+
+const [sortBy, setSortBy] = useState("input");
+
+let sortedItems ;
+
+if(sortBy === "input") sortedItems = items;
+
+if(sortBy === "description") sortedItems = [...items].sort((a, b) => a.description.localeCompare(b.description));
+
+if(sortBy === "packed") sortedItems = [...items].sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return(
   <div className="list">
     <ul >
-      {items.map(item => 
+      {sortedItems.map(item => 
         ( <Item item={item} key={item.id} onDelete={onDelete} itemToggle={itemToggle}/> ))}
     </ul>
+    <div className="actions">
+<select value={sortBy} onChange={e => setSortBy(e.target.value)}>
+  <option value="input">Sort by input</option>
+  <option value="description">Sort by description</option>
+  <option value="packed">Sort by packed</option>
+</select>
+</div>
+
+
+
+
+
   </div>
+
+
   )
 }
 
